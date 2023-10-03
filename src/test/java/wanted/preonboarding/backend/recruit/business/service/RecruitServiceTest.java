1d7e39130c20c4d00eed970a8f97a247b5fc28f3
jkde7721 <jkde7721@naver.com>
2023-10-04 00:06:30 +0900

    test: 채용공고 삭제 서비스 성공 테스트 작성

--- src/test/java/wanted/preonboarding/backend/recruit/business/service/RecruitServiceTest.java
+++ src/test/java/wanted/preonboarding/backend/recruit/business/service/RecruitServiceTest.java
@@ -71,6 +71,22 @@ class RecruitServiceTest {
     }
 
     @Test
+    void removeRecruit() {
+        //given
+        Company company = Company.builder().id(1L).name("원티드랩").nation("한국").region("서울").build();
+        Recruit recruit = Recruit.builder().id(1L).company(company)
+                .position("백엔드 주니어 개발자").compensationFee(1000000L)
+                .details("원티드랩에서 백엔드 주니어 개발자를 채용합니다. 자격요건은..").skills("Python").build();
+        Mockito.when(recruitRepository.findById(anyLong())).thenReturn(Optional.of(recruit));
+
+        //when
+        recruitService.removeRecruit(1L);
+
+        //then
+        verify(recruitRepository, times(1)).delete(recruit);
+    }
+
+    @Test
     void getRecruitFail() {
         Mockito.when(recruitRepository.findById(anyLong())).thenReturn(Optional.empty());
 
