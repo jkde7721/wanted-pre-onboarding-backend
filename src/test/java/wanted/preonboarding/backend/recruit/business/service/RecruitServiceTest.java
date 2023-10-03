9d02c54cd60f36a38517e4d723ccce0b5444bb1a
jkde7721 <jkde7721@naver.com>
2023-10-03 18:40:51 +0900

    test: 채용공고 수정 서비스 성공 테스트 작성

--- src/test/java/wanted/preonboarding/backend/recruit/business/service/RecruitServiceTest.java
+++ src/test/java/wanted/preonboarding/backend/recruit/business/service/RecruitServiceTest.java
@@ -6,9 +6,8 @@ import org.mockito.*;
 import org.mockito.junit.jupiter.MockitoExtension;
 import wanted.preonboarding.backend.company.business.service.CompanyService;
 import wanted.preonboarding.backend.company.persistence.entity.Company;
-import wanted.preonboarding.backend.exception.BusinessException;
-import wanted.preonboarding.backend.exception.ErrorCode;
-import wanted.preonboarding.backend.recruit.business.dto.request.RecruitSaveRequest;
+import wanted.preonboarding.backend.exception.*;
+import wanted.preonboarding.backend.recruit.business.dto.request.*;
 import wanted.preonboarding.backend.recruit.persistence.entity.Recruit;
 import wanted.preonboarding.backend.recruit.persistence.repository.RecruitRepository;
 
@@ -49,6 +48,26 @@ class RecruitServiceTest {
         assertThat(recruitId).isEqualTo(recruit.getId());
         verify(companyService, times(1)).getCompany(anyLong());
         verify(recruitRepository, times(1)).save(any(Recruit.class));
+    }
+
+    @Test
+    void modifyRecruit() {
+        //given
+        Company company = Company.builder().id(1L).name("원티드랩").nation("한국").region("서울").build();
+        Recruit recruit = Recruit.builder().id(1L).company(company)
+                .position("백엔드 주니어 개발자").compensationFee(1000000L)
+                .details("원티드랩에서 백엔드 주니어 개발자를 채용합니다. 자격요건은..").skills("Python").build();
+        Mockito.when(recruitRepository.findById(anyLong())).thenReturn(Optional.of(recruit));
+
+        //when
+        RecruitModifyRequest recruitModifyRequest = new RecruitModifyRequest("백엔드 시니어 개발자", 1500000L, "네이버에서 백엔드 시니어 개발자를 채용합니다.", "Spring");
+        recruitService.modifyRecruit(1L, recruitModifyRequest);
+
+        //then
+        assertThat(recruit)
+                .extracting("position", "compensationFee", "details", "skills")
+                .containsExactly(recruitModifyRequest.getPosition(), recruitModifyRequest.getCompensationFee(), recruitModifyRequest.getDetails(), recruitModifyRequest.getSkills());
+        verify(recruitRepository, times(1)).findById(anyLong());
     }
 
     @Test
