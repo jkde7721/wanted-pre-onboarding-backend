127c00c0ae04c2a87e6a409d93826f8580c136d5
jkde7721 <jkde7721@naver.com>
2023-10-10 18:24:36 +0900

    feat: 채용공고 지원 API 구현

---
+++ src/main/java/wanted/preonboarding/backend/apply/web/dto/response/ApplyCreateResponse.java
@@ -0,0 +1,11 @@
+package wanted.preonboarding.backend.apply.web.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class ApplyCreateResponse {
+
+    private Long applyId;
+}
