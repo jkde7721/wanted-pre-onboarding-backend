167679d90822bc116a55f2213720038283b40abb
jkde7721 <jkde7721@naver.com>
2023-10-11 21:54:40 +0900

    add: id로 회사 조회하는 서비스 메소드에 트랜잭션 어노테이션 추가

--- src/main/java/wanted/preonboarding/backend/domain/company/business/service/CompanyService.java
+++ src/main/java/wanted/preonboarding/backend/domain/company/business/service/CompanyService.java
@@ -2,6 +2,7 @@ package wanted.preonboarding.backend.domain.company.business.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import wanted.preonboarding.backend.domain.company.persistence.entity.Company;
 import wanted.preonboarding.backend.domain.company.persistence.repository.CompanyRepository;
 import wanted.preonboarding.backend.global.exception.BusinessException;
@@ -14,6 +15,7 @@ public class CompanyService {
 
     private final CompanyRepository companyRepository;
 
+    @Transactional(readOnly = true)
     public Company getCompany(Long companyId) {
         return companyRepository.findById(companyId)
                 .orElseThrow(() -> new BusinessException(COMPANY_NOT_FOUND));
